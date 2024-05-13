2173425e38224956afce5d5a53b084e0328721e5
rizvan <rizvan.achmad@itenas.ac.id>
2024-05-13 09:27:37 +0700

    login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="css/styleguide.css"/>
+    <link rel="stylesheet" href="css/style.css"/>
+  </head>
+  <body>
+    <div class="element">
+      <div class="div">
+        <img class="group" src="assets/group-3.png" />
+        <div class="overlap-group">
+          <button class="primary-button">Masuk</button>
+          <img class="img" src="assets/group-3-2.png" />
+          <div class="frame"><div class="text-wrapper-2">Username</div></div>
+          <div class="div-wrapper"><div class="text-wrapper-2">Password</div></div>
+          <input class="rectangle"></input>
+          <input class="rectangle-2" type="password" name="password"></input>
+          <input type="checkbox" class="rectangle-3" >
+          <div class="frame-2"><div class="text-wrapper-3">Remember me</div></div>
+        </div>
+        <div class="navbar">
+          <div class="frame-3"></div>
+          <div class="group-wrapper"><img class="group-2" src="assets/group-3-2.png" /></div>
+          <div class="navbar-2">
+            <div class="text-wrapper-4">Beranda</div>
+            <div class="text-wrapper-4">Profil</div>
+            <div class="text-wrapper-4">Layanan</div>
+            <div class="text-wrapper-4">Dokumen</div>
+            <img class="vector" src="asset/vector-1.svg" />
+          </div>
+          <img class="vector-2" src="asset/vector-1.svg" />
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
